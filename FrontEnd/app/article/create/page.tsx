7a4ce89cee7188c8cb59413316220d2edd9683a7
FMonitor <13723423785@163.com>
2025-05-25 11:27:42 +0800

    构建自动部署流水线，修复前端组件错误导致无法Build问题

--- FrontEnd/app/article/create/page.tsx
+++ FrontEnd/app/article/create/page.tsx
@@ -109,15 +109,9 @@ export default function CreateArticlePage() {
           onChange={(e) => setCategory(e.target.value)}
           required
         >
-          <SelectItem value="technology" key={"1"}>
-            科技
-          </SelectItem>
-          <SelectItem value="science" key={"2"}>
-            科学
-          </SelectItem>
-          <SelectItem value="lifestyle" key={"3"}>
-            生活方式
-          </SelectItem>
+          <SelectItem key="technology">科技</SelectItem>
+          <SelectItem key="science">科学</SelectItem>
+          <SelectItem key="lifestyle">生活方式</SelectItem>
         </Select>
         <div>
           <label className="block mb-2">封面图片</label>
--- FrontEnd/app/competition/create/page.tsx
+++ FrontEnd/app/competition/create/page.tsx
@@ -154,10 +154,10 @@ function CreateCompetitionPage() {
           onChange={(e) => setCompetitionType(e.target.value)}
           required
         >
-          <SelectItem value="hackathon" key={"hackathon"}>黑客马拉松</SelectItem>
-          <SelectItem value="datascience" key={"datascience"}>数据科学</SelectItem>
-          <SelectItem value="ai" key={"ai"}>人工智能</SelectItem>
-          <SelectItem value="programming" key={"programming"}>编程竞赛</SelectItem>
+          <SelectItem key="hackathon">黑客马拉松</SelectItem>
+          <SelectItem key="datascience">数据科学</SelectItem>
+          <SelectItem key="ai" >人工智能</SelectItem>
+          <SelectItem key="programming">编程竞赛</SelectItem>
         </Select>
 
         <Input
