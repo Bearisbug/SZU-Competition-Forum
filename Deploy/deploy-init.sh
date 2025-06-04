0dd456bd72dc5b218312ea5f36e04fa44e3676d9
FMonitor <13723423785@163.com>
2025-06-04 19:30:31 +0800

    fix deployment issues

--- Deploy/deploy-init.sh
+++ Deploy/deploy-init.sh
@@ -8,18 +8,21 @@ PROJECT_ROOT="$( cd "$SCRIPT_DIR/.." &> /dev/null && pwd )"
 cd "$PROJECT_ROOT"
 echo "当前工作目录：$(pwd)"
 
+# echo "--------STEP 1/8: 准备 Docker 离线安装包...-------"
+# bash ./Deploy/download-docker-offline.sh
+
 # 设置国内源
-echo "--------STEP 1/6: 更新 APT 源--------"
+echo "--------STEP 2/8: 更新 APT 源--------"
 sudo apt-get update
 sudo apt-get install -y curl gnupg ca-certificates lsb-release git apt-transport-https software-properties-common
 
 # 添加 Docker
-echo "--------STEP 2/6: 添加 Docker --------"
+echo "--------STEP 3/8: 添加 Docker --------"
 sudo apt-get install -y docker.io docker-compose 
 sudo systemctl start docker
 
 # 设置 Docker 国内镜像源
-echo "--------STEP 3/6: 配置 Docker 加速镜像--------"
+echo "--------STEP 4/8: 配置 Docker 加速镜像--------"
 sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json > /dev/null <<EOF
 {
@@ -40,15 +43,26 @@ docker-compose version
 
 mkdir -p Deploy/build
 
-echo "--------STEP 4/6: 构建前端镜像并保存...-------"
+echo "--------STEP 5/8: 构建前端镜像并保存...-------"
 docker build -t deploy-frontend -f ./Deploy/frontend/Dockerfile ./FrontEnd
 docker save deploy-frontend -o ./Deploy/build/deploy-frontend.tar
 
-echo "--------STEP 5/6: 构建后端镜像并保存...-------"
+echo "--------STEP 6/8: 构建后端镜像并保存...-------"
 docker build -t deploy-backend -f ./Deploy/backend/Dockerfile ./BackEnd
 docker save deploy-backend -o ./Deploy/build/deploy-backend.tar
 
-echo "--------STEP 6/6: 启动 Docker Compose 服务...-------"
+echo "--------STEP 7/8: 拉取并保存依赖镜像...-------"
+# 拉取 nginx 镜像
+echo "拉取 nginx:stable 镜像..."
+docker pull nginx:stable
+docker save nginx:stable -o ./Deploy/build/nginx-stable.tar
+
+# 拉取 redis 镜像
+echo "拉取 redis:alpine 镜像..."
+docker pull redis:alpine
+docker save redis:alpine -o ./Deploy/build/redis-alpine.tar
+
+echo "--------STEP 8/8: 启动 Docker Compose 服务...-------"
 docker-compose -f 'Deploy/docker-compose.yml' --project-name 'deploy' down
 docker-compose -f 'Deploy/docker-compose.yml' --project-name 'deploy' up -d
 
--- FrontEnd/CONFIG.ts
+++ FrontEnd/CONFIG.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL =  'http://172.31.69.242';
+export const API_BASE_URL =  'http://172.31.234.146';
 export const IMAGE_DOMAINS = ['s1.locimg.com', new URL(API_BASE_URL).hostname];
