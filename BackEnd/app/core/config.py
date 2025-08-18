7b524aebed6d761cbe26dbacf08b61e0c3158d68
Chill Bug <109200659+Bearisbug@users.noreply.github.com>
2025-08-18 15:54:51 +0800

    「后端」更新文件错误

--- BackEnd/app/core/config.py
+++ BackEnd/app/core/config.py
@@ -9,7 +9,7 @@ import os
 
 # JWT 密钥
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your_secret_key")
- Token 过期时间（分钟）
+# Token 过期时间（分钟）
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 720))
 # 数据库配置（SQLAlchemy）
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database.db")
